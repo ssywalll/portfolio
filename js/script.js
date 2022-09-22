89177cf5d16efa9cb9c8869d6054f983e2ef6311
ssywalll <noorsyawal6@gmail.com>
2022-09-22 19:49:36 +0800

    update

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ const header = document.querySelector('.header');
 window.addEventListener('scroll', function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const nama = document.querySelector(".form-control.name").value;
+const email = document.querySelector(".form-control.email").value;
+const message = document.querySelector(".form-control.message").value;
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "noorsyawal6@gmail.com",
+        Password : "bengbeng112",
+        To : 'noorsyawal6@gmail.com',
+        From : nama,
+        Subject : email,
+        Body : `nama : ${nama} <br/> Email : ${email} <br/> Message : ${message}`
+    }).then(
+        message => alert("message sent successfully")
+    );
+}
+
+
+console.log(sendEmail(email.send()));
